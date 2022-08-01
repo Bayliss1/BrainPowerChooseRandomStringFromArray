5380cb8cafcad02301aba3ed6622a33f554b073f
Owen <owen.bayliss@sky.com>
2022-08-01 23:03:20 +0100

    Add project files.

---
+++ BrainPowerChooseRandomStringFromArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BrainPowerChooseRandomStringFromArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            string[] myArray;
+            myArray = new string[6];
+            myArray[3] = "Test 3";
+            myArray[5] = "Test 5";
+
+            Console.WriteLine(myArray[(random.Next(6))]);
+        }
+    }
+}
